bbc4d325758dc91f16a48da4d3e5f6d2d955e2b0
陈元广_windows <837990335@qq.com>
2018-06-06 14:59:51 +0800

    sd

---
+++ react/src/main.js
@@ -0,0 +1,54 @@
+import React,{Component} from "react"
+import ReactDOM from "react-dom"
+import { setInterval } from "timers";
+
+// 无状态组件
+// function render(){
+//     const element = <h2>{new Date().toLocaleTimeString()}.</h2>
+//     ReactDOM.render(
+//         element,
+//         document.getElementById('root')
+//     );
+
+// }
+// setInterval(render,1000)
+
+// react采用createClass方式进行组件的注册
+// console.log(React)
+// const Test=React.createClass({
+//     // getInitialState:function(){
+//     //   return {
+//     //     name:"zhangsan"
+//     //   }
+//     // },
+      
+//     // getDefaultProps:function(){
+//     //   return {
+//     //     name:"zhangsan" //这里的zhangsan相当于默认值
+//     //   }
+//     // }
+//     render(){
+//         return <h2>啊哈哈</h2>
+//     }
+//   })
+
+//   ReactDOM.render(
+//     <Test />,
+//     document.getElementById('root')
+// );
+
+
+class App extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <h2>哈哈</h2>
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
